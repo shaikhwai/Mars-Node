795d508d3ec885bcef7d09406d9a9704b6982aad
sudhakarphule1 <sudhakar.phule@twinprimelabs.com>
2016-04-18 17:00:06 +0530

    resolved CORS issue using npm-cors package

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@
 
     import express = require("express");
     import Middlewares = require("./config/middlewares/base/MiddlewaresBase");
+
+    import cors = require('cors')
     
     var app = express();
+    app.use(cors());
     var port = parseInt(process.env.PORT, 10) || 5000;
     app.set("port", port);
     app.use(Middlewares.configuration);
